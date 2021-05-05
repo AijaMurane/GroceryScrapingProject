1c2fe077d731cd33c7f3fc606412cfb07605ab50
Aija <aija.murane@gmail.com>
2021-05-05 16:16:58 +0300

    Getting data from one site

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+import csv
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-URL = 'https://barbora.lv/produkti/mandelu-dzeriens-bez-cukura-alpro-1-l'
-page = requests.get(URL)
-content = page.content
 
-soup = BeautifulSoup(content, 'html.parser')
 
-h3_tag = soup.find('h3', {"class": "b-product-info--info-3-title"})
-h3_name = h3_tag.text
+def getNutritionDataFromBarbora(Barbora_url):
+    page = requests.get(Barbora_url)
+    content = page.content
+    soup = BeautifulSoup(content, 'html.parser')
 
-food_table = soup.find(class_="table table-striped table-condensed")
+    # Finding name of the product
+    title = soup.find('meta', {'name': 'og:title'})
+    product_name = title['content'] if title else 'No meta title given'
 
-rows = food_table.findChildren(['tr'])
+    # Finding the full path of categories
+    category_name_full_path = \
+    json.loads(soup.find('div', {'class': 'b-product-info b-product--js-hook'})['data-b-for-cart'])[
+        'category_name_full_path']
 
-for row in rows:
-    cells = row.findChildren('td', {"class": "b-text-right"})
-    for cell in cells:
-        value = cell.string
-        print("The value in this cell is %s" % value)
+    # Finding values
+    food_table = soup.find(class_="table table-striped table-condensed")
+    rows = food_table.findChildren(['tr'])
+    n_value = []
+    for row in rows:
+        values = row.findChildren('td', {"class": "b-text-right"})
+        for value in values:
+            n_value.append(value.string)
+
+    return [product_name] + [category_name_full_path] + n_value
+
+def writeBarboraDatatoFile(Barbora_list):
+    with open('sample.csv', 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(Barbora_list)
+
+def main():
+    writeBarboraDatatoFile(getNutritionDataFromBarbora('https://barbora.lv/produkti/mandelu-dzeriens-bez-cukura-alpro-1-l'))
+
+if __name__ == "__main__":
+    main()
+
